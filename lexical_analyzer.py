81da9a12e36109842f99ca012b13867ed1b09fe1
AbdElAziz Moftah <vb.promax@gmail.com>
2018-10-29 01:40:36 +0200

    Refactoring the code

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,33 +1,15 @@
 import re
 
-class Token:
-    """ The token complier will takes a dict of tokens, compine them together
-        and reutrn the complied valid re expression."""
-
-    def __init__(self, tokens_dict):
-        self._expression = "".join([str(tokens_dict[k]) for k in tokens_dict])
-        self.pattern = re.compile(self._expression)
-
-    def __str__(self):
-        return self._expression
-
-
 class LaxicalAnalyzer:
     """LaxicalAnalyzer will take a string and split it into tokens"""
-    def __init__(self, compiled_tokens_dict):
-        self._tokens = [(k, compiled_tokens_dict[k]) for k in compiled_tokens_dict]
-
-    def append_complied_tokens(self, compiled_tokens):
-        tmp = [(k, compiled_tokens_dict[k]) for k in compiled_tokens_dict]
-        for t in tmp:
-            self._tokens.append(t)
+    def __init__(self, tokens_dict):
+        self._tokens = [(k, re.compile(tokens_dict[k])) for k in tokens_dict]
 
     def analyze(self, string):
         string = string.split()
-
         for word in string:
-            for token_name, token in self._tokens:
-                match = re.match(token.pattern, word)
+            for token_name, pattern in self._tokens:
+                match = re.match(pattern, word)
                 if match and (match.end() - match.start() == len(word)):
-                    print(token_name, word)
+                    print("%s\t Type: %s" % (word, token_name))
                     break
--- tokens-detector.py
+++ tokens-detector.py
@@ -1,39 +1,19 @@
 import re
 from lexical_analyzer import *
 
-t_keywords = Token({"Reserved Words": r'|'.join(['int', 'float'])})
+t_keywords = r'|'.join(['int', 'float'])
 
-t_int = Token({"Digits": r'[0-9]+'})
+t_int = r'[0-9]+'
+t_float = r'[-+]?' + t_int + r'\.' + t_int
 
-t_float = Token({"Sign": r'[-+]?',
-                "Intgers" : t_int,
-                "FloatingPoint" : r'\.',
-                "decimals" : t_int,
-                })
+t_var = r'[_a-zA-Z]+[_a-zA-Z0-9]*'
+t_opr = r'[-+/*=<>]=?'
 
 x = LaxicalAnalyzer({
             "Keyword"   : t_keywords,
+            "Variable"  : t_var,
             "Intger"    : t_int,
-            "Float"     : t_float})
+            "Float"     : t_float,
+            "Operator"  : t_opr})
 
 x.analyze(input())
-
-
-
-
-# var_types = ['int', 'float', 'double', 'long', 'char', 'short', 'string']
-# cmp_types = ['if', 'else', 'elif', 'while', 'for', 'try', 'except']
-#
-# token_key = re.compile(r'|'.join(var_types + cmp_types))
-#
-# token_int = re.compile(r'[0-9]+')
-# token_var = re.compile(r'[_a-zA-Z]+[_a-zA-Z0-9]*')
-#
-#
-# string = input()
-#
-# matchs = re.finditer(token_var, string)
-#
-#
-# for value in matchs:
-#     print(value)
